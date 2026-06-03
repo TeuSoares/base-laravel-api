574c763cc54de85411ca582705bb40ef0c6eb8b6
Mateus <mateussoares114@gmail.com>
2026-06-03 20:43:06 -0300

    feat: add lifetime plan to payment

--- config/payment.php
+++ config/payment.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'gateway' => env('PAYMENT_GATEWAY', 'stripe'),
+    'gateway'      => env('PAYMENT_GATEWAY', 'stripe'),
+    'default_plan' => env('PAYMENT_DEFAULT_PLAN', 'monthly'),
 
     'plans' => [
-        'monthly' => env('PAYMENT_MONTHLY_PLAN'),
-        'yearly'  => env('PAYMENT_YEARLY_PLAN'),
+        'monthly'  => env('PAYMENT_MONTHLY_PLAN'),
+        'yearly'   => env('PAYMENT_YEARLY_PLAN'),
+        'lifetime' => env('PAYMENT_LIFETIME_PLAN'),
     ],
-
-    'default_plan' => env('PAYMENT_DEFAULT_PLAN', 'monthly'),
 ];
