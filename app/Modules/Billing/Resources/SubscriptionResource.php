5ab73511f4640fce4e170c2d9d086ebc7e71b912
Mateus <mateussoares114@gmail.com>
2026-06-18 20:33:46 -0300

    feat: add period of plan

--- app/Modules/Billing/Resources/SubscriptionResource.php
+++ app/Modules/Billing/Resources/SubscriptionResource.php
@@ -12,13 +12,18 @@ class SubscriptionResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $stripePrice = $this->resource->stripe_price;
+
+        $period = array_search($stripePrice, config('payment.plans', [])) ?: 'monthly';
+
         return [
-            'plan' => $this->resource->stripe_price,
+            'plan' => $stripePrice,
+            'period' => $period,
             'status' => $this->resource->stripe_status,
             'ends_at' => $this->resource->ends_at?->toIso8601String(),
             'trial_ends' => $this->resource->trial_ends_at?->toIso8601String(),
             'on_trial' => $this->resource->onTrial(),
-            'cancelled' => $this->resource->cancelled(),
+            'canceled' => $this->resource->canceled(),
             'active' => $this->resource->active(),
         ];
     }
